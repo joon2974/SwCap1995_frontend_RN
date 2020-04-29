52436e4efa7d0e991370b2e1163bcf1106f17fea
joon2974 <vnfmsdyd4@naver.com>
2020-04-29 22:48:29 +0900

    warning 처리, signupscreen 수정, axios 적용

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import {View, StyleSheet, ActivityIndicator} from 'react-native';
 import NavigationScreen from './Components/NavigateScreen';
+import ErrorBoundary from './ErrorBoundary';
 
 export default class App extends Component{
  
   render(){
       return(
-        <NavigationScreen />
+        <ErrorBoundary>
+          <NavigationScreen />
+        </ErrorBoundary>
       )
   }
 }
