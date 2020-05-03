235a4652c6010d1f32a3acf4b9a4430ea8031221
joon2974 <vnfmsdyd4@naver.com>
2020-05-03 21:38:19 +0900

    sign up api test

--- ErrorBoundary.js
+++ ErrorBoundary.js
@@ -4,6 +4,7 @@ export default class ErrorBoundary extends React.Component {
     state = { hasError: false }
   
     static getDerivedStateFromError (error) {
+      console.log(error)
       return { hasError: true }
     }
   
