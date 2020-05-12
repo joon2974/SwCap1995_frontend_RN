e953c8d0d02de62079043ee81baac5607ea80794
hansuchan <suchan7311@naver.com>
2020-05-12 23:19:07 +0900

    get friend list

--- Components/FriendScreens/FriendScreen.js
+++ Components/FriendScreens/FriendScreen.js
@@ -5,30 +5,53 @@ import {
   Button,
 } from 'react-native';
 import axios from 'axios';
-
+import FriendList from './FriendComponents/FriendList';
   
 export default class FriendScreen extends Component {
-  getFriends = (userID) => {
-    axios.get('http://49.50.172.58:3000/friends', {
-      params: { userID },
-    })
-      .then(function (response) {
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+  constructor(props) {
+    super(props);
+    this.state = {
+      friendData: [],
+    };
   }
 
+  componentDidMount() { 
+    this.getFriends(1);
+  }
+
+  getFriends = async (userID) => {
+    try {
+      console.log('시작');
+      const response = await axios.get('http://49.50.172.58:3000/friends/' + userID);
+      console.log(response.data.rows);
+      
+      const responseJson = await response.data.rows;
+
+      await this.setState({
+        friendData: responseJson,
+      });
+    } catch (error) {
+      console.error(error);
+      console.error('오류');
+    }
+  }
+
+
   render() {
+    const friends = this.state.friendData.map((data) => (
+      <FriendList
+        nickname={data.nickname}
+        />
+    ));
     return (
       <View style={styles.container}>
         <Button
-          style={{ marginTop: 10 }}
-          title="친구 목록받아오기"
-          onPress={() => this.getFriends(1)
-            }
-           />
+          title="친구추가"
+          onPress={() => this.props.navigation.navigate('AddFriend')}
+        />
+        <View>
+          {friends}
+        </View>
       </View>
     );
   }
