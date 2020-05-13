5bc45c7aa7dcc79e71fe0bdb0887ea7770d5e581
hansuchan <suchan7311@naver.com>
2020-05-13 20:19:07 +0900

    friendrequestlist ui

--- Components/FriendScreens/FriendScreen.js
+++ Components/FriendScreens/FriendScreen.js
@@ -3,15 +3,19 @@ import {
   View,
   StyleSheet,
   Button,
+  Dimensions,
 } from 'react-native';
 import axios from 'axios';
 import FriendList from './FriendComponents/FriendList';
+import FriendRequestList from './FriendComponents/FriendRequestList';
   
+const { width } = Dimensions.get('window');
 export default class FriendScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
       friendData: [],
+      friendRequstData: [],
     };
   }
 
@@ -20,19 +24,31 @@ export default class FriendScreen extends Component {
   }
 
   getFriends = async (userID) => {
+    const response = await axios.get('http://49.50.172.58:3000/friends/' + userID);
+    const responseJson = await response.data.rows;
+    const count = await response.data.count;
+    var friendarray;
+    var friendRequestarray;
+    console.log(count);
     try {
-      console.log('시작');
-      const response = await axios.get('http://49.50.172.58:3000/friends/' + userID);
-      console.log(response.data.rows);
-      
-      const responseJson = await response.data.rows;
-
-      await this.setState({
-        friendData: responseJson,
-      });
+      for (var i = 0; i < 3; i++) {
+        if (responseJson[i].friend[0].isaccept === 'accept') {
+          const obj = { nickname: responseJson[i].nickname };
+          friendarray = this.state.friendData.concat(obj);
+          this.setState({
+            friendData: friendarray,
+          });
+          console.log(this.state.friendData);
+        } else {
+          const obj = { nickname: responseJson[i].nickname };
+          friendRequestarray = this.state.friendRequstData.concat(obj);
+          this.setState({
+            friendRequstData: friendRequestarray,
+          });
+        }
+      }
     } catch (error) {
       console.error(error);
-      console.error('오류');
     }
   }
 
@@ -43,9 +59,18 @@ export default class FriendScreen extends Component {
         nickname={data.nickname}
         />
     ));
+    const friendRequsts = this.state.friendRequstData.map((data) => (
+      <FriendRequestList
+        nickname={data.nickname}
+        />
+    ));
     return (
       <View style={styles.container}>
+        <View>
+          {friendRequsts}
+        </View>
         <Button
+          buttonStyle={styles.addFriendButton}
           title="친구추가"
           onPress={() => this.props.navigation.navigate('AddFriend')}
         />
@@ -61,7 +86,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: 'white',
+  },
+  addFriendButton: {
+    width: width,
   },
 });
