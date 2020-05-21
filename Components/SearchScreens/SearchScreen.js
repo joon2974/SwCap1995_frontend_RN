386a29f4a6445b69d65717a7a224588efd32e7b3
SeongGyun_Park <atagkim@ajou.ac.kr>
2020-05-21 15:33:27 +0900

    추천탭추가

--- Components/SearchScreens/SearchScreen.js
+++ Components/SearchScreens/SearchScreen.js
@@ -1,40 +1,58 @@
 import React, {Component} from 'react';
-import {View, Text, ScrollView, FlatList, StyleSheet, Image, TouchableOpacity, Dimensions} from 'react-native';
-import { Input, Button } from 'react-native-elements';
+import {
+  View, 
+  Text, 
+  ScrollView, 
+  StyleSheet, 
+  Image, 
+  TouchableOpacity, 
+  Dimensions,
+  Button,
+  AsyncStorage} from 'react-native';
+import { Input } from 'react-native-elements';
 import axios from 'axios';
+import PlanListEach from './PlanListEach';
 
-
-//const { width } = Dimensions.get('window');
+const { height, width } = Dimensions.get('window');
 
 export default class Searchscreen extends Component {
 
   state = {
+    selectedCategory : '',
+    temp:'abc',
     search: '',
-    temp : 'abc',
+    uri:'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg',
   };
 
-  updateSearch = search => {
-    this.setState({ search });
-  }; 
 
-  sendSearch = (temp) => {
-    axios.get('http://49.50.172.58:3000/categories/search?query=' + temp ).then(res => {
-      console.log(res);
-      alert(res.data);
-    }).catch(error => {
-      console.log(error);
-      alert(error);
-    });
+  updateSearch = (changedSearch)=> {
+    this.setState({ search : changedSearch }); 
+  }
+
+  //graphql?query={categoryGet{id,name,image_url}}
+  async sendSearch(){
+      await axios.get('http://49.50.172.58:3000/graphql?query={categoryGet{id,name,description,image_url}}').then(res => {
+        console.log(res);
+        alert(res.data);
+      }).catch(error => {
+        console.log(error);
+        alert(error);
+      });
+
+      this.setState({uri:'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'});
+  }
+
+
+  componentDidMount() {
+    this.setPlanList('운동/건강');
+  }
+
+  setPlanList = (categoryName) => {
+    this.setState({ selectedCategory: categoryName });
   }
 
 
   render(){
-   const { search } = this.state;
-
-    let pic = {
-      //uri : search
-      uri : 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    }
 
     return (
 
@@ -49,22 +67,17 @@ export default class Searchscreen extends Component {
         <View style = {styles.searchBar}>
           <Input
             placeholder="Type Here..."
-            onChangeText={this.updateSearch}
+            onChangeText={(changedSearch) => {this.updateSearch(changedSearch)}}
           />  
           <Button
             title="검색"
             type="solid"
-            onPress={this.sendSearch}
+            onPress={() => {this.sendSearch()}}
           />
         </View>
 
-
         <ScrollView
           ref={(scrollView) => { this.scrollView = scrollView; }} 
-          //decelerationRate={0}
-          //snapToInterval={width - 60}
-          //snapToAlignment={"center"}
-          //pagingEnabled={true}
           contentInset={{
             top: 0,
             left: 30,
@@ -82,24 +95,64 @@ export default class Searchscreen extends Component {
             </Text>
           </View>
 
+ 
+
+          <View style={styles.tabRecommended}>
+                <TouchableOpacity
+                  style={this.state.selectedCategory === '운동/건강' ? styles.selectedCategoryBtnStyle : styles.categoryBtnStyle}
+                  onPress={() => this.setPlanList('운동/건강')}
+                >
+                  <Text>운동/건강</Text>
+                </TouchableOpacity>     
+
+              <TouchableOpacity
+                  style={this.state.selectedCategory === '감정관리' ? styles.selectedCategoryBtnStyle : styles.categoryBtnStyle}
+                  onPress={() => this.setPlanList('감정관리')}
+                >
+                  <Text>감정관리</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  style={this.state.selectedCategory === '생활습관' ? styles.selectedCategoryBtnStyle : styles.categoryBtnStyle}
+                  onPress={() => this.setPlanList('생활습관')}
+                >
+                  <Text>생활습관</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  style={this.state.selectedCategory === '자기계발' ? styles.selectedCategoryBtnStyle : styles.categoryBtnStyle}
+                  onPress={() => this.setPlanList('자기계발')}
+                >
+                  <Text>자기계발</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  style={this.state.selectedCategory === '기타' ? styles.selectedCategoryBtnStyle : styles.categoryBtnStyle}
+                  onPress={() => this.setPlanList('기타')}
+                >
+                  <Text>기타</Text>
+                </TouchableOpacity>
+                
+            </View>
+
+
         
           <View style={styles.categoryUnitList}>
             <TouchableOpacity style={styles.category} onPress = {() => this.props.navigation.navigate('PlanSearched')} >
-              <Image source = {pic} style = {styles.category} />
+              <Image source = {this.state} style = {styles.category} />
             </TouchableOpacity>
             <TouchableOpacity style={styles.category} onPress = {() => this.props.navigation.navigate('PlanSearched')} >
-              <Image source = {pic} style = {styles.category} />
+              <Image source = {this.state} style = {styles.category} />
             </TouchableOpacity>
           </View>
           <View style={styles.categoryUnitList}>
             <TouchableOpacity style={styles.category} onPress = {() => this.props.navigation.navigate('PlanSearched')} >
-              <Image source = {pic} style = {styles.category} />
+              <Image source = {this.state} style = {styles.category} />
             </TouchableOpacity>
             <TouchableOpacity style={styles.category} onPress = {() => this.props.navigation.navigate('PlanSearched')} >
-              <Image source = {pic} style = {styles.category} />
+              <Image source = {this.state} style = {styles.category} />
             </TouchableOpacity>
           </View>
-
 
           <View>
             <Text style = {styles.recommendTitle}>
@@ -112,29 +165,28 @@ export default class Searchscreen extends Component {
 
           <View style={styles.categoryUnitList}>
             <TouchableOpacity style={styles.category} onPress = {() => this.props.navigation.navigate('PlanSearched')} >
-              <Image source = {pic} style = {styles.category} />
+              <Image source = {this.state} style = {styles.category} />
             </TouchableOpacity>
             <TouchableOpacity style={styles.category} onPress = {() => this.props.navigation.navigate('PlanSearched')} >
-              <Image source = {pic} style = {styles.category} />
+              <Image source = {this.state} style = {styles.category} />
             </TouchableOpacity>
           </View>
           <View style={styles.categoryUnitList}>
             <TouchableOpacity style={styles.category} onPress = {() => this.props.navigation.navigate('PlanSearched')} >
-              <Image source = {pic} style = {styles.category} />
+              <Image source = {this.state} style = {styles.category} />
             </TouchableOpacity>
             <TouchableOpacity style={styles.category} onPress = {() => this.props.navigation.navigate('PlanSearched')} >
-              <Image source = {pic} style = {styles.category} />
+              <Image source = {this.state} style = {styles.category} />
             </TouchableOpacity>
           </View>
 
-
         </ScrollView>
-
 
       </View>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
 
@@ -144,7 +196,7 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
   },
   searchBar:{
-    width:350, 
+    width: width * 0.87, 
     flexDirection:'row'
   },
   searchTitle:{
@@ -167,7 +219,338 @@ const styles = StyleSheet.create({
   },
   category:{
     margin:5,
-    width:180,
+    width:width*0.45,
     height:150,
   },
+
+
+  tabRecommended: {
+    width: width,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  categoryBtnStyle: {
+    width: width * 0.17,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    margin: 1,
+  },
+  selectedCategoryBtnStyle: {
+    width: width * 0.17,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F2F2F2',
+    borderRadius: 5,
+    margin: 1,
+  },
+  
+  
 })
+
+/* 
+
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Dimensions,
+  TouchableOpacity,
+  AsyncStorage,
+  ScrollView,
+} from 'react-native';
+import axios from 'axios';
+import PlanListEach from './PlanComponents/PlanListEach';
+
+let isRegisterdCheck;
+const { height, width } = Dimensions.get('window');
+const plansListTemp = {
+  '운동/건강': [
+    '헬스',
+    '런닝',
+    '푸시업',
+    '기타 등등',
+  ],
+  '감정관리': [
+    '하루 한 번 웃기',
+    '하루 한 번 하늘 보기',
+    'a',
+    'b',
+  ],
+  '생활습관': [
+    '하루 물 한컵 마시기',
+    'q',
+    'w',
+    'e',
+  ],
+  '자기계발': [
+    '아침 10시 일어나기',
+    '아침 9시 일어나기',
+    '12시 전 자기',
+    '스마트폰 이용 줄이기',
+  ],
+  '기타': [
+    '그 외 기타 등등',
+  ],
+};
+
+export default class PlanMain extends Component {
+  state = {
+    isFaceRegisterd: false,
+    selectedCategory: '운동',
+    nowPlanList: ['test', 'data', 'hello', 'world'],
+  } 
+
+  componentDidMount() {
+    this.loadUserID();
+    this.setPlanList('운동/건강');
+  }
+
+  componentWillUnmount() {
+    clearTimeout(isRegisterdCheck);
+  }
+
+  loadUserID = async () => {
+    await AsyncStorage.getItem('UserID')
+      .then((id) => { 
+        this.isFaceRegisterdCheck(id);
+      }); 
+  }
+
+  isFaceRegisterdCheck = (ID) => {
+    console.log('asdfasdfasdf', ID);
+    isRegisterdCheck = axios
+      .get('http://49.50.172.58:3000/users/is_face_detection/' + ID, {
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded',
+        },
+      })
+      .then((res) => {
+        if (res === true) {
+          console.log('얼굴인식 완료된 유저');
+          this.setState({ isFaceRegisterd: true });
+          this.forceUpdate();
+        } else {
+          console.log(res);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  registerComplete = () => {
+    this.setState({ isFaceRegisterd: true });
+  }
+
+  // Test
+  testTurnBack = () => {
+    this.setState({ isFaceRegisterd: false });
+  }
+
+  setPlanList = (categoryName) => {
+    this.setState({ selectedCategory: categoryName });
+    const planList = plansListTemp[categoryName];
+    console.log(planList);
+    this.setState({ nowPlanList: planList });
+  }
+
+  planSelected = (planName) => {
+    this.props.navigation.navigate('MakePlanStep1', { planName: planName });
+  }
+
+  render() {
+    const { isFaceRegisterd, selectedCategory, nowPlanList } = this.state;
+
+    if (isFaceRegisterd) {
+      return (
+        <ScrollView>
+          <View style={styles.container}>
+            <View style={styles.testContainer}>
+              <Text>플랜 메인</Text>
+              <Button title="플랜 생성" onPress={() => this.props.navigation.navigate('MakePlanStep1')} />
+              <Button title="테스트 전환" onPress={() => this.testTurnBack()} />
+            </View>
+
+            <View style={styles.selectionContainer}>
+              <View style={styles.categoryBtnContainer}>
+                <TouchableOpacity
+                  style={selectedCategory === '운동/건강' ? styles.selectedCategoryBtnStyle : styles.categoryBtnStyle}
+                  onPress={() => this.setPlanList('운동/건강')}
+                >
+                  <Text>운동/건강</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  style={selectedCategory === '감정관리' ? styles.selectedCategoryBtnStyle : styles.categoryBtnStyle}
+                  onPress={() => this.setPlanList('감정관리')}
+                >
+                  <Text>감정관리</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  style={selectedCategory === '생활습관' ? styles.selectedCategoryBtnStyle : styles.categoryBtnStyle}
+                  onPress={() => this.setPlanList('생활습관')}
+                >
+                  <Text>생활습관</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  style={selectedCategory === '자기계발' ? styles.selectedCategoryBtnStyle : styles.categoryBtnStyle}
+                  onPress={() => this.setPlanList('자기계발')}
+                >
+                  <Text>자기계발</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  style={selectedCategory === '기타' ? styles.selectedCategoryBtnStyle : styles.categoryBtnStyle}
+                  onPress={() => this.setPlanList('기타')}
+                >
+                  <Text>기타</Text>
+                </TouchableOpacity>
+                
+              </View>
+              <View style={styles.plansContainer}>
+                {nowPlanList.map((data) => (
+                  <PlanListEach
+                    key={data}
+                    name={data}
+                    planSelectFunc={() => this.planSelected(data)}
+                  />
+                ))}
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <View style={styles.InfoContainer}>
+            <View style={styles.informTitle}>
+              <Text style={{ fontWeight: 'bold', fontSize: 30 }}>얼굴 인증</Text>
+            </View>
+            <View style={styles.information}>
+              <Text>Plan A는 사용자의 대리 인증을 방지하기 위해 본인 인증을 위한 수단으로 얼굴 인증을 사용하고 있습니다.</Text>
+              <Text>하단의 두 방법 중 하나의 방법으로 자신의 얼굴을 Plan A에 등록해 주세요.</Text>
+            </View>
+            <Button 
+              title="테스트 전환"
+              onPress={() => this.registerComplete()}
+            />
+          </View>
+          <View style={styles.BtnContainer}>
+            <TouchableOpacity 
+              style={styles.registerBtn}
+              onPress={() => this.props.navigation.navigate('CameraScreen', { completeFunc: this.registerComplete.bind(this) })}
+            >
+              <Text>카메라로 사진 찍기</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+              style={styles.registerBtn}
+              onPress={() => this.props.navigation.navigate('ImagePickScreen', { completeFunc: this.registerComplete.bind(this) })}
+            >
+              <Text>갤러리에서 사진 가져오기</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  testContainer: {
+    width: width,
+    height: 70,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  selectionContainer: {
+    width: width,
+    height: height,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  InfoContainer: {
+    height: height / 2,
+    width: width,
+    justifyContent: 'center',
+  },
+  categoryBtnContainer: {
+    width: width,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  categoryBtnStyle: {
+    width: width * 0.17,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    margin: 1,
+  },
+  selectedCategoryBtnStyle: {
+    width: width * 0.17,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F2F2F2',
+    borderRadius: 5,
+    margin: 1,
+  },
+  plansContainer: {
+    width: width,
+    height: height - 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  BtnContainer: {
+    height: height / 2,
+    width: width,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  informTitle: {
+    height: height / 4,
+    width: width,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  information: {
+    height: height / 4,
+    width: width - 30,
+    justifyContent: 'center',
+    marginLeft: 15,
+  },
+  registerBtn: {
+    width: 130,
+    height: 130,
+    borderRadius: 20,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 20,
+  },
+});
+*/
+
