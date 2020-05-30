4c365d6507ad777aab27294b425bd822c8bc351f
wltjr0920 <wltjr0920@ajou.ac.kr>
2020-05-30 19:21:27 +0900

    Revert "감시중인 플랜 수정"
    This reverts commit 1ee26d6c346f7fca92e8d3e7090e10d46da0f0f5.

--- Components/HomeScreens/HomeMain.js
+++ Components/HomeScreens/HomeMain.js
@@ -1,38 +1,33 @@
 import React, { Component } from 'react';
-import {
+import { 
   View,
   Text,
   StyleSheet,
   AsyncStorage,
   ActivityIndicator,
   Dimensions,
-  ScrollView,
-  Image,
-  TouchableOpacity,
-  Platform,
-  RefreshControl,
+  ScrollView, Image, TouchableOpacity, Platform, RefreshControl,
 } from 'react-native';
 import firebase from 'firebase';
 import axios from 'axios';
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons'; 
 import { LinearGradient } from 'expo-linear-gradient';
 import InputInfo from '../LogInScreens/InputInfo';
 import MyPlan from '../MyScreens/MyComponents/MyPlan';
-import { Constants } from 'expo-camera';
 
 console.disableYellowBox = true;
 let currentUser;
 let isInformCheck;
 const { width, height } = Dimensions.get('window');
 export default class HomeMain extends Component {
-  state = {
+  state = { 
     isInformChecked: false,
     userEmail: '',
     userId: '',
     planData: [],
     watchData: [],
     refreshing: false,
-  };
+  }; 
 
   componentDidMount() {
     currentUser = firebase.auth().currentUser;
@@ -58,16 +53,16 @@ export default class HomeMain extends Component {
     this.loadAllPlan(this.state.userId).then(() => {
       this.setState({ refreshing: false });
     });
-  };
+  }
 
   // Async 확인용
   checkAsync = async () => {
     const ji = await AsyncStorage.getItem('UserID');
     console.log('asdf', ji);
-  };
+  }
 
   loadUserID = async () => {
-    await AsyncStorage.getItem('UserID').then(id => {
+    await AsyncStorage.getItem('UserID').then((id) => {
       this.state.userId = id;
       this.loadAllPlan(id);
     });
@@ -75,9 +70,9 @@ export default class HomeMain extends Component {
 
   moveToPlan = () => {
     this.props.navigation.dangerouslyGetParent().navigate('Plan');
-  };
+  }
 
-  loadAllPlan = async userId => {
+  loadAllPlan = async (userId) => {
     console.log('유저아이디1', userId);
     const response = await axios.get(
       'http://49.50.172.58:3000/plans/all/' + userId,
@@ -91,8 +86,8 @@ export default class HomeMain extends Component {
         for (var i = 0; i < count; i++) {
           const obj = {
             title: responseJson[i].title,
-            url: responseJson[i].image_url,
-            picturetime: responseJson[i].picture_time,
+            url: responseJson[i].image_url, 
+            picturetime: responseJson[i].picture_time, 
             id: responseJson[i].id,
           };
           planarray = this.state.planData.concat(obj);
@@ -109,14 +104,14 @@ export default class HomeMain extends Component {
     );
     const watchresponseJson = await watchresponse.data.rows;
     const watchcount = await watchresponse.data.count;
-
+    
     try {
       if (count !== 0) {
         for (var l = 0; l < watchcount; l++) {
           const obj = {
             title: watchresponseJson[l].title,
-            url: watchresponseJson[l].image_url,
-            picturetime: watchresponseJson[l].picture_time,
+            url: watchresponseJson[l].image_url, 
+            picturetime: watchresponseJson[l].picture_time, 
             id: watchresponseJson[l].id,
             nickname: watchresponseJson[l].user.nickname,
           };
@@ -133,9 +128,10 @@ export default class HomeMain extends Component {
 
   moveToPlan = () => {
     this.props.navigation.dangerouslyGetParent().navigate('Plan');
-  };
+  }
 
-  isInfoContain = async eMail => {
+
+  isInfoContain = async (eMail) => { 
     isInformCheck = await axios
       .post('http://49.50.172.58:3000/users/is_user', {
         headers: {
@@ -143,7 +139,7 @@ export default class HomeMain extends Component {
         },
         email: eMail,
       })
-      .then(res => {
+      .then((res) => {
         if (res.data.id) {
           console.log('데이터 이미 존재');
           AsyncStorage.setItem('UserID', res.data.id.toString());
@@ -152,14 +148,16 @@ export default class HomeMain extends Component {
           console.log(res);
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
   };
 
   render() {
-    const { isInformChecked, userEmail, planData, watchData } = this.state;
-    const plans = planData.map(data => (
+    const {
+      isInformChecked, userEmail, planData, watchData, 
+    } = this.state;
+    const plans = planData.map((data) => (
       <MyPlan
         key={data.id}
         title={data.title}
@@ -168,7 +166,7 @@ export default class HomeMain extends Component {
         picturetime={data.picturetime}
       />
     ));
-    const watchplans = watchData.map(data => (
+    const watchplans = watchData.map((data) => (
       <MyPlan
         key={data.id}
         title={data.title}
@@ -181,7 +179,7 @@ export default class HomeMain extends Component {
     if (isInformChecked) {
       return (
         <ScrollView
-          refreshControl={
+          refreshControl={(
             <RefreshControl
               refreshing={this.state.refreshing}
               onRefresh={this.onRefresh}
@@ -190,31 +188,29 @@ export default class HomeMain extends Component {
               titleColor="#00ff00"
               colors={['#ff0000', '#00ff00', '#0000ff']}
               progressBackgroundColor="white"
-            />
-          }
+          />
+        )}
         >
-          <LinearGradient
-            colors={['white', '#FBEFEF']}
-            style={styles.container}
-          >
+          <LinearGradient colors={['white', '#FBEFEF']} style={styles.container}>
             <View style={styles.planContainer}>
-              <ScrollView
+              <ScrollView 
                 horizontal={true}
                 showsHorizontalScrollIndicator={false}
                 contentContainerStyle={{
                   alignItems: 'center',
                 }}
+               
               >
                 <View style={{ marginRight: 30, marginLeft: 10 }}>
                   <Text>도전중인</Text>
                   <Text>플랜</Text>
                   <Image
                     source={{
-                      uri:
-                        'https://kr.object.ncloudstorage.com/swcap1995/001-right-arrow-1.png',
+                      uri: 'https://kr.object.ncloudstorage.com/swcap1995/001-right-arrow-1.png',
                     }}
                     style={{ width: 50, height: 50, marginTop: 20 }}
-                  />
+                />
+          
                 </View>
                 {plans}
                 <View style={styles.addContainer}>
@@ -230,47 +226,41 @@ export default class HomeMain extends Component {
             </View>
             <View style={styles.lineDivider} />
             <View style={styles.planContainer}>
-              {watchplans.length > 0 && (
-                <>
-                  <ScrollView>
-                    horizontal={true}
-                    showsHorizontalScrollIndicator={false}
-                    contentContainerStyle=
-                    {{
-                      alignItems: 'center',
-                      paddingEnd: 5,
+              
+              <ScrollView 
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{
+                  alignItems: 'center',
+                  paddingEnd: 5,
+                }}
+              >   
+                <View style={{ marginRight: 30, marginLeft: 10 }}>
+                  <Text>감시중인</Text>
+                  <Text>플랜</Text>
+                  <Image
+                    source={{
+                      uri: 'https://kr.object.ncloudstorage.com/swcap1995/001-right-arrow-1.png',
                     }}
-                    >
-                    <View style={{ marginRight: 30, marginLeft: 10 }}>
-                      <Text>감시중인</Text>
-                      <Text>플랜</Text>
-                      <Image
-                        source={{
-                          uri:
-                            'https://kr.object.ncloudstorage.com/swcap1995/001-right-arrow-1.png',
-                        }}
-                        style={{ width: 50, height: 50, marginTop: 20 }}
-                      />
-                      {watchplans}
-                    </View>
-                  </ScrollView>
-                </>
-              )}
-              {!watchplans.length && (
-                <>
-                  <View style={styles.noplans}>
-                    <Text>감시중인 플랜이 없습니다</Text>
-                  </View>
-                </>
-              )}
+                    style={{ width: 50, height: 50, marginTop: 20 }}
+            />
+                </View>
+      
+                {watchplans}
+                {!(watchplans.length) && (
+                <View style={styles.addContainer}>
+             
+                  <Text>감시중인 플랜이 없습니다</Text>
+                 
+                </View>
+                )}
+              </ScrollView>
             </View>
           </LinearGradient>
         </ScrollView>
       );
     } else if (isInformChecked === false) {
-      return (
-        <InputInfo checkFunc={this.informExistCheck} userEmail={userEmail} />
-      );
+      return <InputInfo checkFunc={this.informExistCheck} userEmail={userEmail} />;
     } else {
       return <ActivityIndicator />;
     }
@@ -280,18 +270,14 @@ export default class HomeMain extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    // backgroundColor: 'white',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
   planContainer: {
     width: width,
-    height: height * 0.43,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    height: height * 0.403,
+  },  
   lineDivider: {
     backgroundColor: '#F2F2F2',
     width: width - 30,
@@ -308,28 +294,6 @@ const styles = StyleSheet.create({
     marginRight: 20,
     justifyContent: 'center',
     alignContent: 'center',
-    ...Platform.select({
-      ios: {
-        shadowColor: 'rgb(50, 50, 50)',
-        shadowOpacity: 0.1,
-        shadowRadius: 5,
-        shadowOffset: {
-          height: -1,
-          width: 0,
-        },
-      },
-      android: {
-        elevation: 4,
-      },
-    }),
-  },
-  noplans: {
-    width: width * 0.7,
-    height: width / 1.6,
-    backgroundColor: 'white',
-    borderRadius: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
     ...Platform.select({
       ios: {
         shadowColor: 'rgb(50, 50, 50)',
