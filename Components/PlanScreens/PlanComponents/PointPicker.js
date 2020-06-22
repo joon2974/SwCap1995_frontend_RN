9f12172591d8879379c5b9f87b92eaeae839071d
wltjr0920 <wltjr0920@ajou.ac.kr>
2020-06-22 18:27:40 +0900

    UI 수정

--- Components/PlanScreens/PlanComponents/PointPicker.js
+++ Components/PlanScreens/PlanComponents/PointPicker.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Picker } from 'react-native';
+import { Picker, View, Dimensions } from 'react-native';
 import propTypes from 'prop-types';
+
+const { width } = Dimensions.get('window');
 
 export default class PointPicker extends Component {
   static propTypes = {
@@ -9,19 +11,36 @@ export default class PointPicker extends Component {
 
   render() {
     return (
-      <Picker
-        selectedValue={this.props.point}
-        onValueChange={this.props.onValueChange}
+      <View
         style={{
-          width: this.props.pickerWidth, height: 45, marginLeft: 5, borderRadius: 15,
+          borderColor: '#FD8A69',
+          borderWidth: 3,
+          borderRadius: 10,
+          width: this.props.pickerWidth,
         }}
-        itemStyle={{ height: 40 }}
-        mode="dropdown"
       >
-        {this.props.points.map((point) => (
-          <Picker.Item key={point} label={point} value={point} style={{ alignText: 'center' }} />
-        ))}
-      </Picker>
+        <Picker
+          selectedValue={this.props.point}
+          onValueChange={this.props.onValueChange}
+          style={{
+            width: this.props.pickerWidth,
+            height: 45,
+            marginLeft: 5,
+            borderRadius: 15,
+          }}
+          itemStyle={{ height: 40 }}
+          mode="dropdown"
+        >
+          {this.props.points.map(point => (
+            <Picker.Item
+              key={point}
+              label={point}
+              value={point}
+              style={{ alignText: 'center' }}
+            />
+          ))}
+        </Picker>
+      </View>
     );
   }
 }
--- Components/PlanScreens/PlanComponents/RulePicker.js
+++ Components/PlanScreens/PlanComponents/RulePicker.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Picker } from 'react-native';
+import { Picker, View, Dimensions } from 'react-native';
 import propTypes from 'prop-types';
 
+const { width } = Dimensions.get('window');
 export default class RulePicker extends Component {
   static propTypes = {
     onValueChange: propTypes.func.isRequired,
@@ -9,17 +10,31 @@ export default class RulePicker extends Component {
 
   render() {
     return (
-      <Picker
-        selectedValue={this.props.rule}
-        onValueChange={this.props.onValueChange}
-        style={{ width: this.props.pickerWidth, height: 50, marginLeft: 5 }}
-        itemStyle={{ height: 40 }}
-        mode="dropdown"
+      <View
+        style={{
+          borderColor: '#FD8A69',
+          borderWidth: 3,
+          borderRadius: 10,
+          width: width - 20,
+        }}
       >
-        {this.props.rules.map((rule) => (
-          <Picker.Item key={rule} label={rule} value={rule} />
-        ))}
-      </Picker>
+        <Picker
+          selectedValue={this.props.rule}
+          onValueChange={this.props.onValueChange}
+          style={{
+            width: this.props.pickerWidth,
+            width: width - 20,
+            height: 50,
+            marginLeft: 5,
+          }}
+          itemStyle={{ height: 40 }}
+          mode="dropdown"
+        >
+          {this.props.rules.map(rule => (
+            <Picker.Item key={rule} label={rule} value={rule} />
+          ))}
+        </Picker>
+      </View>
     );
   }
 }
